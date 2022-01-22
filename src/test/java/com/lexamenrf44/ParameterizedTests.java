45300052692f29c3ae9ff88633e721628fc7932a
Lexamenrf44 <alexander.stcstudent@gmail.com>
2022-01-23 01:33:12 +0300

    Created new repository "lexamenrf44-parameterized-tests", created new files ".gitignore", "build.gradle", "ParameterizedTests.java", edited .gitignore, build.gradle and ParameterizedTests.java in the main branch

---
+++ src/test/java/com/lexamenrf44/ParameterizedTests.java
@@ -0,0 +1,29 @@
+package com.lexamenrf44;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+import static org.openqa.selenium.By.partialLinkText;
+
+public class ParameterizedTests {
+
+    @Test
+    public void issueSearchTest() {
+
+        open("https://github.com");
+
+        $(".header-search-input").click();
+        $(".header-search-input").sendKeys("eroshenkoam/allure-example");
+        $(".header-search-input").submit();
+
+        $(linkText("eroshenkoam/allure-example")).click();
+        $(partialLinkText("Issues")).click();
+        $(withText("#68")).should(Condition.visible);
+
+    }
+
+}
