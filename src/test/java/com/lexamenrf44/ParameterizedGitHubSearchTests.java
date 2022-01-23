62a45b64a899e9f5f920192cd4146862c8d4ad3f
Lexamenrf44 <alexander.stcstudent@gmail.com>
2022-01-23 18:49:50 +0300

    Edited build.gradle, renamed and ParameterizedTests.java into ParameterizedGitHubSearchTests.java, edited ParameterizedGitHubSearchTests.java in the main branch

--- src/test/java/com/lexamenrf44/ParameterizedGitHubSearchTests.java
+++ src/test/java/com/lexamenrf44/ParameterizedGitHubSearchTests.java
@@ -1,7 +1,9 @@
 package com.lexamenrf44;
 
 import com.codeborne.selenide.Condition;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
@@ -9,18 +11,21 @@ import static com.codeborne.selenide.Selenide.open;
 import static org.openqa.selenium.By.linkText;
 import static org.openqa.selenium.By.partialLinkText;
 
-public class ParameterizedTests {
+@DisplayName("Parameterized tests class")
+public class ParameterizedGitHubSearchTests {
 
-    @Test
-    public void issueSearchTest() {
+    @ValueSource(strings = {"eroshenkoam/allure-example"})
+    @ParameterizedTest
+    @DisplayName("Searching gitHub issues")
+    public void issueSearchTest(String testData) {
 
         open("https://github.com");
 
         $(".header-search-input").click();
-        $(".header-search-input").sendKeys("eroshenkoam/allure-example");
+        $(".header-search-input").sendKeys(testData);
         $(".header-search-input").submit();
 
-        $(linkText("eroshenkoam/allure-example")).click();
+        $(linkText(testData)).click();
         $(partialLinkText("Issues")).click();
         $(withText("#68")).should(Condition.visible);
 
---
+++ src/test/java/com/lexamenrf44/SimpleDisabledTest.java
@@ -0,0 +1,17 @@
+package com.lexamenrf44;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+@Disabled
+public class SimpleDisabledTest {
+
+    @Test
+    public void test () {
+        Assertions.assertTrue(3 > 2);
+
+    }
+
+
+}
